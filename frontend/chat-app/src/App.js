be0fd2aabb3efe2591298b9cbb62b20559105912
rohangreddy <rohangreddy@gmail.com>
2021-03-16 13:04:46 -0400

    imported landing page component

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -1,34 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Landing from './Components/landing'
 
-function App() {
-  return (
-    <div>
-        <div class="title">
-            <div class="column is-4">
-                ChatApp
-            </div>
-        </div>
-        
-        <form class="box is-half">
-            <div class="title">Welcome!</div>
-            <div class="field">
-                <label class="label">Email</label>
-                <div class="control">
-                    <input class="input" type="email" placeholder="e.g. alex@example.com"></input>
-                </div>
-            </div>
-            <div class="field">
-                <label class="label">Password</label>
-                <div class="control">
-                    <input class="input" type="password" placeholder="*******"></input>
-                </div>
-            </div>
-            <button class="button is-primary">Login</button>
-        </form>
-        
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Landing></Landing>
+            </div> 
+        );
+    }
 }
 
 export default App;
