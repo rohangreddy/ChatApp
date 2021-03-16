07ab6061666e1fc5dacef03ab16a71ead1586b02
rohangreddy <rohangreddy@gmail.com>
2021-03-16 14:09:25 -0400

    landing page style changes

--- frontend/chat-app/src/Components/landing.jsx
+++ frontend/chat-app/src/Components/landing.jsx
@@ -1,33 +1,51 @@
 import React, { Component } from 'react'
 
+var rootStyle = {
+    backgroundColor: '#363940',
+    color : 'white',
+    height: '100vh',
+    minheight : '100vh'
+}
+
+var boxStyle = {
+    backgroundColor: '#303136',
+    width: '500px'
+}
+
+var fieldStyle = {
+    backgroundColor: '#40454B',
+    color: 'white'
+}
+
+
 export default class Landing extends Component {
     render() {
         return (
-            <div>
+            <div style={rootStyle}>
                 <div class="title">
-                    <div class="column is-4">
+                    <div class="column is-4 has-text-white">
                         ChatApp
                     </div>
                 </div>
                 <div class="columns is-centered">
                     <div class="column is-narrow">
-                        <form class="box">
-                            <div class="column">
-                                <div class="title">Welcome!</div>
+                        <form class="box" style={boxStyle}>
+                            <div class="column is-offset-one-third">
+                                <div class="title has-text-white">Welcome!</div>
                             </div>
                             <div class="field">
-                                <label class="label">Email</label>
+                                <label class="label" style={{ color: '#60636A'}}>Email</label>
                                 <div class="control">
-                                    <input class="input" type="email" placeholder="e.g. alex@example.com"></input>
+                                    <input class="input" type="email" placeholder="e.g. alex@example.com has-text-white" style={fieldStyle}></input>
                                 </div>
                             </div>
                             <div class="field">
-                                <label class="label">Password</label>
+                                <label class="label" style={{ color: '#60636A'}} >Password</label>
                                 <div class="control">
-                                    <input class="input" type="password" placeholder="*******"></input>
+                                    <input class="input" type="password" placeholder="*******" style={fieldStyle}></input>
                                 </div>
                             </div>
-                            <button class="button is-primary">Login</button>
+                                <button class="button" style={{backgroundColor: '#6979F8'}}>Login</button>
                         </form>
                     </div>
                 </div>
