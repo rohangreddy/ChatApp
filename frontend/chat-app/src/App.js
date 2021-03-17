7020b286674678a5693545293c8de486cb4bbb55
rohangreddy <rohangreddy@gmail.com>
2021-03-17 13:02:39 -0400

    added react router

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from './Components/landing'
 import MainPage from './Components/main'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Landing></Landing>
-                <MainPage></MainPage>
-            </div> 
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path ="/" component={Landing} />
+                    <Route path="/main" component={MainPage} />
+                </Switch>
+            </BrowserRouter> 
         );
     }
 }
