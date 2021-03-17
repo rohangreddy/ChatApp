b68b01b141859f0e8651d9fbb90d353a08ddb2b7
rohangreddy <rohangreddy@gmail.com>
2021-03-17 12:12:54 -0400

    added MainPage component

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
 import Landing from './Components/landing'
-
+import MainPage from './Components/main'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Landing></Landing>
+                <MainPage></MainPage>
             </div> 
         );
     }
