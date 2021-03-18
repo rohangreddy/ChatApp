8706841278d85bf26911b91832581f6281183f1c
rohangreddy <rohangreddy@gmail.com>
2021-03-18 17:22:34 -0400

    formatted main page

--- frontend/chat-app/src/Components/main.jsx
+++ frontend/chat-app/src/Components/main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 var rootStyle = {
-    backgroundColor: '#363940',
+    backgroundColor: '#212226',
     color : 'white',
     height: '100vh',
     minheight : '100vh'
@@ -16,8 +16,21 @@ export default class MainPage extends Component {
                         ChatApp
                     </div>
                 </div>
+                <div class="columns">
+                    <div class="column is-narrow" style={{ backgroundColor: '#303136', height: '85vh', width: '300px' }}>
+                        <div class="field">
+                            <button class="button is-fullwidth" style={{backgroundColor: '#6979F8'}}>Friends</button>
+                            </div>
+                        <div class="field">
+                            <button class="button is-fullwidth" style={{backgroundColor: '#6979F8'}}>Messages</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         );
     }
 }
+
+
+
