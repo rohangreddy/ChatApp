77a76007f6534eb8fb4f43a631a506699e2029c4
rohangreddy <rohangreddy@gmail.com>
2021-03-18 19:27:05 -0400

    design changes

--- frontend/chat-app/src/Components/main.jsx
+++ frontend/chat-app/src/Components/main.jsx
@@ -3,8 +3,7 @@ import React, { Component } from 'react'
 var rootStyle = {
     backgroundColor: '#212226',
     color : 'white',
-    height: '100vh',
-    minheight : '100vh'
+    height: '100vh'
 }
 
 export default class MainPage extends Component {
@@ -19,11 +18,14 @@ export default class MainPage extends Component {
                 <div class="columns">
                     <div class="column is-narrow" style={{ backgroundColor: '#303136', height: '85vh', width: '300px' }}>
                         <div class="field">
-                            <button class="button is-fullwidth" style={{backgroundColor: '#6979F8'}}>Friends</button>
+                            <button class="button is-fullwidth" style={{backgroundColor: '#40454B', color: '#FFFFFF'}}>Friends</button>
                             </div>
                         <div class="field">
-                            <button class="button is-fullwidth" style={{backgroundColor: '#6979F8'}}>Messages</button>
+                            <button class="button is-fullwidth" style={{backgroundColor: '#40454B', color: '#FFFFFF'}}>Messages</button>
                         </div>
+                    </div>
+                    <div class="column" style={{backgroundColor: '#363940', height: '100vh'}}>
+                            <h1>Dashboard</h1>
                     </div>
                 </div>
             </div>
