ac4695209fc36634bc14c64588b535034dd1745b
rohangreddy <rohangreddy@gmail.com>
2021-03-22 13:10:49 -0400

    added firebase config

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -5,6 +5,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from './Components/landing'
 import MainPage from './Components/main'
 
+var firebaseConfig = {
+    apiKey: "AIzaSyDvQQjZjggLRGGXYhKccshhR8-GD8bheEY",
+    authDomain: "chatapp-96c03.firebaseapp.com",
+    projectId: "chatapp-96c03",
+    storageBucket: "chatapp-96c03.appspot.com",
+    messagingSenderId: "763312119498",
+    appId: "1:763312119498:web:a1d1f020e1406e10e7de33"
+  };
+
 class App extends Component {
     render() {
         return (
