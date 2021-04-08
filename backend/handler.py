3864cc9eb6a7190b54d8ade97069633b9786e293
rohangreddy <rohangreddy@gmail.com>
2021-04-08 14:33:53 -0400

    set up GET requests from test file

--- backend/handler.py
+++ backend/handler.py
@@ -1,4 +1,38 @@
 import json
+import os
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+
+path = 'C:/Users/rohan/Documents/Cornell-Tech/Spring-2021/CS-5356/ChatApp/backend/'
+
+def auth(token):
+    request = requests.Request()
+    if not token:
+        return {
+            "statusCode": 401
+        }
+    id_info = id_token.verify_firebase_token(token, request)
+    if not id_info:
+        return {
+            "statusCode": 401
+        }
+    userid = id_info['sub']
+
+def chats(event, context):
+    if event["path"] == "/chats" and event['httpMethod'] == "GET":
+        
+        token = event["headers"]["Authorization"]
+        auth(token)
+        filename = os.path.join(path, 'test.json')
+        with open(filename) as file:
+            data = json.load(file)
+            return {
+                "statusCode": 200,
+                "body": json.dumps(data)
+            }
+
+
 
 def hello(event, context):
     if event["httpMethod"] == "GET" and event["path"] == "/whoami":
@@ -19,6 +53,8 @@ def hello(event, context):
         }
 
     return response 
+
+
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
